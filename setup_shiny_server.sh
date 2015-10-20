0bda27b739c8aaaa7d634ab227c8ef22dcfde5f6
michbur <michalburdukiewicz@gmail.com>
2015-10-20 10:03:20 +0200

    finished configuration

--- setup_shiny_server.sh
+++ setup_shiny_server.sh
@@ -6,3 +6,10 @@ sudo apt-get install r-base-dev
 sudo apt-get install gdebi-core
 sudo gdebi shiny-server-1.4.0.deb
 sudo su - -c "R -e \"install.packages('shiny')\""
+sudo su - -c "R -e \"install.packages('devtools')\""
+sudo su - -c "R -e \"install.packages('devtools')\""
+sudo su - -c "R -e \"devtools::install_github('michbur/dpcR')\""
+echo "cat(system.file(package = 'dpcR'), '/dpcReport\n', sep = '')" | R --no-save > tmp_file
+tail -2 tmp_file | head -1 | cp shiny-server
+cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/shiny-server
+rm tmp_file
