252769abb2aa0c843c426186b38f0e1f8c584da8
michbur <michalburdukiewicz@gmail.com>
2015-10-20 14:26:34 +0200

    ypdated installation guidelines

--- setup_shiny_server.sh
+++ setup_shiny_server.sh
@@ -3,13 +3,19 @@ sudo apt-get update
 sudo apt-get install r-base
 sudo apt-get install r-base-dev
 sudo apt-get install gdebi-core
-sudo gdebi shiny-server-1.4.0.deb
-sudo su - -c "R -e \"install.packages('shiny')\""
-sudo su - -c "R -e \"install.packages('devtools')\""
-sudo su - -c "R -e \"install.packages('rmarkdown')\""
-sudo su - -c "R -e \"devtools::install_github('michbur/dpcR')\""
+sudo apt-get install libxml2-dev
+sudo apt-get install libx11-dev 
+sudo apt-get install mesa-common-dev
+sudo apt-get install libglu1-mesa-dev
+wget http://download2.rstudio.org/rstudio-server-0.97.336-amd64.deb
+wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.0.756-amd64.deb
+sudo gdebi shiny-server-1.4.0.756-amd64.deb
+sudo su shiny
+# sudo su - -c "R -e \"install.packages('shiny')\""
+# sudo su - -c "R -e \"install.packages('devtools')\""
+# sudo su - -c "R -e \"install.packages('rmarkdown')\""
+# sudo su - -c "R -e \"devtools::install_github('michbur/dpcR')\""
 echo "cat(system.file(package = 'dpcR'), '/dpcReport\n', sep = '')" | R --no-save > tmp_file
-tail -2 tmp_file | head -1 | cp shiny-server
-cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/shiny-server
+sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/shiny-server
 rm tmp_file
 #address http://123.456.1.2:3838/
