914dfe2d5fce6a8d9c3854a6d675a815e2040874
michbur <michalburdukiewicz@gmail.com>
2015-12-10 10:07:39 +0100

    added cran2roxygen

---
+++ cran2roxygen.R
@@ -0,0 +1,11 @@
+# R function to covert suggestions from R ("consider adding blah blah blah") to roxygen
+library(dplyr)
+
+readLines("cran2roxygen_example.txt") %>%
+  gsub(",", "", .) %>%
+  gsub("(", " ", ., fixed = TRUE) %>%
+  gsub(")", " ", ., fixed = TRUE) %>%
+  gsub(")", " ", ., fixed = TRUE) %>%
+  gsub("[ ]{2,}", "", .) %>%
+  gsub("importFrom", "\n#' @importFrom", .) %>%
+  cat(., file = "result.txt")
