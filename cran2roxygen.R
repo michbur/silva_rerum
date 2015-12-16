18c3f59f85abdedc66270722dd191edc6e612f3e
michbur <michalburdukiewicz@gmail.com>
2015-12-16 11:03:39 +0100

    added example

--- cran2roxygen.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-
-readLines("cran2roxygen_example.txt") %>%
-  gsub('"', " ", ., fixed = TRUE) %>%
-  gsub("[(),]", " ", .) %>%
-  gsub("[ ]{1,}", " ", .) %>%
-  gsub("^ ", "", .) %>%
-  gsub("importFrom", "\n#' @importFrom", .) %>%
-  cat(., sep = "", file = "result.txt")
