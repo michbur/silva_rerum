89de5de2e899f0343b24a1aa2c9a28465c5cfc06
michbur <michalburdukiewicz@gmail.com>
2016-02-26 12:53:51 +0100

    added new fix

--- update_signalHsmm.sh
+++ update_signalHsmm.sh
@@ -8,3 +8,6 @@ echo "cat(system.file(package = 'signalHsmm'), '/signal_gui\n', sep = '')" | R -
 sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/signalHsmm
 sudo start shiny-server
 #address http://smorfland.uni.wroc.pl:3838/signalHsmm
+
+# change server file
+sudo wget -O /srv/shiny-server/signalHsmm/server.R "http://raw.githubusercontent.com/michbur/signalHsmm/master/inst/signal_gui/server.R"
