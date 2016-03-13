bf8e26fa0efd4a848c40a283bf3bcb2618e92552
michbur <michalburdukiewicz@gmail.com>
2016-03-13 11:21:18 +0100

    updated my theme

--- my_ggplot_theme.R
+++ my_ggplot_theme.R
@@ -1,20 +1,33 @@
 #my favorite theme for ggplot2
 library(ggplot2)
-library(reshape2)
-library(dplyr)
+library(grid)
+library(gridExtra)
 
 size_mod <- -4
-cool_theme <- theme(plot.background=element_rect(fill = "transparent",
-                                                 colour = "transparent"),
-                    panel.grid.major = element_line(colour="lightgrey", linetype = "dashed"),
-                    panel.background = element_rect(fill = "white",colour = "black"),
-                    legend.background = element_rect(fill="NA"),
-                    legend.position = "right",
-                    axis.text = element_text(size=12 + size_mod),
-                    axis.title.x = element_text(size=16 + size_mod, vjust = -1), 
-                    axis.title.y = element_text(size=16 + size_mod, vjust = 1),
-                    strip.text = element_text(size=12 + size_mod, face = "bold"),
-                    strip.background = element_rect(fill="grey", colour = "black"),
-                    legend.text = element_text(size=13 + size_mod), 
-                    legend.title = element_text(size=17 + size_mod),
-                    plot.title = element_text(size=20 + size_mod))
+
+my_theme <- theme(
+  axis.text = element_text(size=13 + size_mod),
+  axis.title.x = element_text(size=14 + size_mod, vjust = -1),
+  axis.title.y = element_text(size=14 + size_mod, vjust = 1),
+  
+  legend.background = element_rect(fill = "NA"),
+  legend.key = element_rect(fill = "NA", color = "NA", size = 0.5),
+  legend.position = "bottom",
+  legend.key.size = unit(0.1, "inches"),
+  legend.margin = unit(-0.25, "lines"),
+  legend.text = element_text(size=13 + size_mod), 
+  legend.title = element_text(size=14 + size_mod),
+  
+  panel.grid.major = element_line(color="grey", linetype = "dashed", size = 0.5),
+  panel.grid.major = element_line(color="lightgrey", 
+                                  linetype = "dashed", size = 0.5),
+  panel.background = element_rect(fill = "transparent", color = "black"),
+  
+  plot.background=element_rect(fill = "transparent",
+                               color = "transparent"),
+  plot.margin = unit(rep(0.02, 4), "inches"),
+  plot.title = element_text(size=20 + size_mod),
+  
+  strip.background = element_rect(fill = "NA", color = "NA"),
+  strip.text = element_text(size=13 + size_mod, face = "bold")
+)
