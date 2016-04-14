babbfe3bd626ec4a7d2e4100dc5c20cc1eb2fc2a
michbur <michalburdukiewicz@gmail.com>
2016-04-14 13:09:51 +0200

    added my R configuration

---
+++ R_config/.Rprofile.R
@@ -0,0 +1,31 @@
+print.data.frame <- function (x, ..., digits = NULL, quote = FALSE, right = TRUE, 
+                              row.names = TRUE, head = TRUE) 
+{
+  n <- length(row.names(x))
+  if (length(x) == 0L) {
+    cat(sprintf(ngettext(n, "data frame with 0 columns and %d row", 
+                         "data frame with 0 columns and %d rows", domain = "R-base"), 
+                n), "\n", sep = "")
+  }
+  else if (n == 0L) {
+    print.default(names(x), quote = FALSE)
+    cat(gettext("<0 rows> (or 0-length row.names)\n"))
+  }
+  else {
+    m <- as.matrix(format.data.frame(x, digits = digits, 
+                                     na.encode = FALSE))
+    if (!isTRUE(row.names)) 
+      dimnames(m)[[1L]] <- if (identical(row.names, FALSE)) 
+        rep.int("", n)
+    else row.names
+    
+    if(head) {
+      print(head(m), ..., quote = quote, right = right)
+      cat("----\n", nrow(m) - 5, "rows ommited\n")
+    } else {
+      print(m, ..., quote = quote, right = right)
+    }
+  }
+  invisible(x)
+}
+
