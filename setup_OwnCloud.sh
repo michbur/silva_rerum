f248a614e8b75e1cebdcacf7b6db7c50484433e6
michbur <michalburdukiewicz@gmail.com>
2016-07-25 10:17:46 +0200

    added OwnCloud

---
+++ setup_OwnCloud.sh
@@ -0,0 +1,31 @@
+#<?php
+#$processUser = posix_getpwuid(posix_geteuid());
+#echo "User: " . $processUser['name'];
+#$processGroup = posix_getgrgid($processUser['gid']);
+#echo " Group: " . $processGroup['name'];
+#?>
+
+# zapisać jako /var/www/html/whoami.php
+
+#script for setting permissions
+
+#!/bin/bash
+ocpath='/var/www/owncloud'
+htuser='www-data'
+htgroup='www-data'
+rootuser='root' # On QNAP this is admin
+
+find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
+find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+
+chown -R ${rootuser}:${htgroup} ${ocpath}/
+chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+chown -R ${htuser}:${htgroup} ${ocpath}/config/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+
+chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+
+chmod 0644 ${ocpath}/.htaccess
+chmod 0644 ${ocpath}/data/.htaccess
