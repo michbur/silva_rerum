ac254b31f269efc7a83a982939b797236ec9af89
michbur <michalburdukiewicz@gmail.com>
2016-11-28 09:44:36 +0100

    correct address

--- setup_period_app.sh
+++ setup_period_app.sh
@@ -1,5 +1,5 @@
 cd /srv/shiny-server/
-git clone https://github.com/michbur/silva_rerum.git period_app
+git clone https://github.com/michbur/period.git period_app
 sudo su shiny
 sudo su - -c "R -e \"install.packages(c('qpcR', 'lmtest', 'lawstat', 'gplots'))\"
 # check http://www.smorfland.uni.wroc.pl/shiny/AmyloGram/
