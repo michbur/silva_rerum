5da5f91911906890585b74ccd4f5c3e393f8a6ee
michbur <michalburdukiewicz@gmail.com>
2017-01-05 15:26:57 +0100

    update functions updated

--- install_dpcReport.sh
+++ install_dpcReport.sh
@@ -1,5 +1,9 @@
-sudo R -e "devtools::install_github('michbur/dpcR')"
-echo "cat(system.file(package = 'dpcR'), '/dpcReport\n', sep = '')" | R --no-save > tmp_file
-sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/dpcReport
+su shiny
 
-#address http://smorfland.uni.wroc.pl/shiny/dpcReport
+R -e "devtools::install_github('michbur/dpcR')"
+echo "cat(system.file(package = 'dpcR'), '/dpcReport\n', sep = '')" | R --no-save > tmp_file
+cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/dpcReport
+
+R -e "devtools::install_github('michbur/AmyloGram')"
+echo "cat(system.file(package = 'AmyloGram'), '/AmyloGram\n', sep = '')" | R --no-save > tmp_file
+cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/AmyloGram
