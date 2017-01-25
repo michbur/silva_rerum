4a02ba29c31330c9d6ca0df0473e041005185ed0
michbur <michalburdukiewicz@gmail.com>
2017-01-25 13:35:16 +0100

    added sequence generation

--- nginx_conf.sh
+++ nginx_conf.sh
@@ -7,3 +7,13 @@ sudo nano /etc/nginx/sites-enabled/default
 #        }
 
 sudo service nginx restart
+
+sudo stop shiny-server
+sudo su shiny
+R
+
+library(devtools)
+install_github("michbur/AmyloGram")
+
+echo "cat(system.file(package = 'AmyloGram'), '/AmyloGram\n', sep = '')" | R --no-save > tmp_file
+sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/AmyloGram
