54f5601e55ea01f363583808dfd9bce7388c3147
michbur <michalburdukiewicz@gmail.com>
2017-05-28 22:36:36 +0200

    more universal pubs miner

--- pubs_miner.R
+++ pubs_miner.R
@@ -1,5 +1,4 @@
 library(rentrez)
-arch, rettype = "uilist")
 
 search_year <- function(quotation, year){
   query <- paste(quotation, "AND (", year, "[PDAT])")
@@ -10,13 +9,11 @@ search_year <- function(quotation, year){
 
 year <- 1990:2017
 pubs <- sapply(year, search_year, quotation = "peptide array[TIAB] OR peptide microarray[TIAB])")
-dpcr_pubs <- data.frame(year = year, n_pub = pubs)
+pubs <- data.frame(year = year, n_pub = pubs)
 
-mod <- nls(n_pub ~ exp(a + b * year), data = dpcr_pubs[-nrow(dpcr_pubs), ], start = list(a = 0, b = 0))
+mod <- nls(n_pub ~ exp(a + b * year), data = pubs[-nrow(pubs), ], start = list(a = 0, b = 0))
 
-final_pubs <- cbind(dpcr_pubs, pred = predict(mod, list(year = dpcr_pubs[["year"]]))) %>% 
+final_pubs <- cbind(pubs, pred = predict(mod, list(year = pubs[["year"]]))) %>% 
   melt(id.vars = "year") %>% 
   mutate(value = ifelse(variable == "pred" & year != 2017, 0, value)) %>% 
   mutate(variable = factor(variable, labels = c("Observed", "Predicted")))
-
-write.csv(final_pubs, file = "./data/dpcr_final_data.csv", row.names = FALSE)
