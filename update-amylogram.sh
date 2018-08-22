a370aeeeda1f59bddc48749f305006979fe1e6d7
michbur <michalburdukiewicz@gmail.com>
2018-08-22 15:32:46 +0200

    fixing things

--- update-amylogram.sh
+++ update-amylogram.sh
@@ -1,8 +1,8 @@
 sudo stop shiny-server
 sudo su shiny
-R
-source("https://install-github.me/michbur/AmyloGram")
+echo "source("https://install-github.me/michbur/AmyloGram")" | R --no-save 
 echo "cat(system.file(package = 'AmyloGram'), '/AmyloGram\n', sep = '')" | R --no-save > tmp_file
+exit
 rm -rf /srv/shiny-server/AmyloGram
 sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/AmyloGram
 sudo start shiny-server
