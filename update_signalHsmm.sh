70a2ed36e3ff1eaf28f6bac94f442d8b04a4d291
michbur <michalburdukiewicz@gmail.com>
2018-11-12 09:43:17 +0100

    updated update script

--- update_signalHsmm.sh
+++ update_signalHsmm.sh
@@ -1,14 +1,4 @@
-sudo stop shiny-server
-sudo su shiny
-R
-install.packages(c("rmarkdown", "devtools", "shiny", "DT"))
-library(devtools)
-install_github("michbur/signalHsmm")
+sudo su - -c "R -e \"install.packages(c('shinythemes', 'rmarkdown'))\""
+sudo su - -c "R -e \"source('https://install-github.me/michbur/signalHsmm')\""
 echo "cat(system.file(package = 'signalHsmm'), '/signal_gui\n', sep = '')" | R --no-save > tmp_file
 sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/signalHsmm
-sudo start shiny-server
-#address http://smorfland.uni.wroc.pl:3838/signalHsmm
-
-# change server file
-sudo wget -O /srv/shiny-server/signalHsmm/server.R "http://raw.githubusercontent.com/michbur/signalHsmm/master/inst/signal_gui/server.R"
-sudo cp -R AmyloGram/ /srv/shiny-server/AmyloGram
