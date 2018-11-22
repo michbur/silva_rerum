29d8e8b058da835a18818fbea9bb16ee14821de1
michbur <michalburdukiewicz@gmail.com>
2018-11-22 13:08:11 +0100

    updated code

--- setup_reverseR.sh
+++ setup_reverseR.sh
@@ -1,4 +1,5 @@
 sudo su - -c "R -e \"source('https://install-github.me/anspiess/reverseR')\""
 echo "cat(system.file(package = 'reverseR'), '/reverseR\n', sep = '')" | R --no-save > tmp_file
+sudo rm -rf /srv/shiny-server/reverseR
 sudo cp -R `tail -2 tmp_file | head -1` /srv/shiny-server/reverseR
 # sudo systemctl restart shiny-server
