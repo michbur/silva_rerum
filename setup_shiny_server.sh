bd106b069d993915ccc9b757fcd919208af72da5
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-12-04 09:50:18 +0100

    updated for R 4.0

--- setup_shiny_server.sh
+++ setup_shiny_server.sh
@@ -1,19 +1,17 @@
 #cat /etc/*-release
-sudo sh -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/" >> /etc/apt/sources.list'
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt install r-base
 sudo apt-get update
 sudo apt-get install r-base
 sudo apt-get install gdebi-core
-sudo apt-get install libxml2-dev
-sudo apt-get install libx11-dev 
-sudo apt-get install mesa-common-dev
-sudo apt-get install libglu1-mesa-dev
-sudo apt-get install libssl-dev
-sudo apt-get install libgmp3-dev
+sudo apt-get install gdebi-core libxml2-dev libx11-dev mesa-common-dev libglu1-mesa-dev libssl-dev libgmp3-dev
+wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.13.944-amd64.deb
 sudo su - \
 -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
-wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.13.944-amd64.deb
-sudo gdebi shiny-server-1.5.13.944-amd64.deb
+wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.15.953-amd64.deb
+sudo gdebi shiny-server-1.5.15.953-amd64.deb
 sudo su shiny
 # sudo su - -c "R -e \"install.packages('shiny')\""
 # sudo su - -c "R -e \"install.packages('devtools')\""
