91e848f360f68cc55044cfd44426e094cf3ba29f
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2022-11-01 08:43:07 +0100

    forgotten about library

--- complete-example-that-I-would-actually-remember.R
+++ complete-example-that-I-would-actually-remember.R
@@ -1,3 +1,5 @@
+library(tidyr)
+
 rbind(data.frame(word = c("a", "b"), count = c(5, 10), prop = c(0.2, 0.4), 
                  type = "tool"),
       data.frame(word = "a", count = 10, prop = 0.4, type = "non-tool")) %>% 
