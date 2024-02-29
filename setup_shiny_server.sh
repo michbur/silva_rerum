20f73e9b6eadc9a998c25e9d288f910f64445991
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2024-02-29 22:52:10 +0100

    Update setup_shiny_server.sh

--- setup_shiny_server.sh
+++ setup_shiny_server.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-
 sudo apt-get update
 sudo apt-get install r-base
 sudo apt-get install gdebi-core
-sudo apt-get install gdebi-core cmake libxml2-dev libcurl4-openssl-dev libx11-dev mesa-common-dev libglu1-mesa-dev libssl-dev libgmp3-dev zlib1g-dev libjpeg-dev libpng-dev
+sudo apt-get install gdebi-core cmake libxml2-dev libcurl4-openssl-dev libx11-dev mesa-common-dev libglu1-mesa-dev libssl-dev libgmp3-dev zlib1g-dev libjpeg-dev libpng-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
 sudo su - \
 -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
 wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.17.973-amd64.deb
